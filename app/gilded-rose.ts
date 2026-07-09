9794a0c89a148ba56e5f9c791296b316e0c0dce7
DimRad26 <DimitrieOctavian.Radu@softwire.com>
2026-07-09 14:06:00 +0300

    Unit Testing - T4esting IsBounded to make sure

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,6 +17,10 @@ export class GildedRose {
         this.items = items;
     }
 
+    private inBounds(quality: number) : boolean {
+        return quality > 0 && quality < 50;
+    }
+
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
             // Handle Sulfuras
@@ -27,20 +31,20 @@ export class GildedRose {
             this.items[i].sellIn = this.items[i].sellIn - 1;
 
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
+                if (this.inBounds(this.items[i].quality)) {
                     this.items[i].quality = this.items[i].quality - 1;
                 }
             } else {
-                if (this.items[i].quality < 50) {
+                if (this.inBounds(this.items[i].quality)) {
                     this.items[i].quality = this.items[i].quality + 1
                     if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].sellIn < 10) {
-                            if (this.items[i].quality < 50) {
+                            if (this.inBounds(this.items[i].quality)) {
                                 this.items[i].quality = this.items[i].quality + 1
                             }
                         }
                         if (this.items[i].sellIn < 5) {
-                            if (this.items[i].quality < 50) {
+                            if (this.inBounds(this.items[i].quality)) {
                                 this.items[i].quality = this.items[i].quality + 1
                             }
                         }
@@ -51,13 +55,13 @@ export class GildedRose {
             // Handle Expired Items
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name == 'Aged Brie') {
-                    if (this.items[i].quality < 50) {
+                    if (this.inBounds(this.items[i].quality)) {
                         this.items[i].quality = this.items[i].quality + 1
                     }
                 } else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
                     this.items[i].quality = 0;
                 } else {
-                    if (this.items[i].quality > 0) {
+                    if (this.inBounds(this.items[i].quality)) {
                         this.items[i].quality = this.items[i].quality - 1;
                     }
                 }
